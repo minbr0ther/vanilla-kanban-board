22806ff79e538fc924918e40f98a56c8fdbdeace
minbr0ther <minbr0ther@naver.com>
2022-03-09 17:03:17 +0900

    feat: item click & update 구현

--- js/view/Item.js
+++ js/view/Item.js
@@ -1,3 +1,5 @@
+import KanbanAPI from "../api/KanbanAPI.js";
+
 export default class Item {
   constructor(id, content) {
     this.elements = {};
@@ -9,6 +11,20 @@ export default class Item {
     this.elements.root.dataset.id = id;
     this.elements.input.textContent = content;
     this.content = content;
+
+    const onBlur = () => {
+      // onBlur가 발생 시점의 textContent를 가져온다
+      const newContent = this.elements.input.textContent.trim();
+
+      if (this.content == newContent) return;
+
+      this.content = newContent;
+      KanbanAPI.updateItem(id, {
+        content: this.content,
+      });
+    };
+
+    this.elements.input.addEventListener("blur", onBlur);
   }
 
   static createRoot() {
@@ -16,8 +32,7 @@ export default class Item {
 
     return range.createContextualFragment(`
         <div class="kanban__item" draggable="true">
-            <div class="kanban__item-input" contenteditable>
-            
+            <div class="kanban__item-input" contenteditable="true">
             </div>
         </div>
     `).children[0];

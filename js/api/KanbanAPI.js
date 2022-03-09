db33b8d5ab417645bc509e713cd16369ec6f901a
minbr0ther <minbr0ther@naver.com>
2022-03-09 14:34:51 +0900

    feat: delete 구현

--- js/api/KanbanAPI.js
+++ js/api/KanbanAPI.js
@@ -73,6 +73,18 @@ export default class KanbanAPI {
 
     save(data);
   }
+
+  static deleteItem(itemId) {
+    const data = read();
+
+    for (const column of data) {
+      const item = column.items.find((item) => item.id === itemId);
+
+      item && column.items.splice(column.items.indexOf(item), 1);
+    }
+
+    save(data);
+  }
 }
 
 function read() {
